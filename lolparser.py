a2033ab5284723623680a60b85756fb7575e9538
Oscar Kilhed <oscar.kilhed@gmail.com>
2011-12-28 19:00:17 +0100

    finished IOC refactoring and switched from pyquery to BeautifulSoup since pyquery  wouldn't compile on osx

--- lolparser.py
+++ lolparser.py
@@ -1,6 +1,8 @@
 import requests
+import datetime
+import re
 from pymongo import Connection
-from pyquery import PyQuery as pq
+from BeautifulSoup import BeautifulSoup
 
 class Player:
   def __init__(self, name, rank, wins, losses, rating):
@@ -9,49 +11,50 @@ class Player:
     self.wins = wins
     self.losses = losses
     self.rating = rating
-  
+    self.parsedDate = datetime.datetime.utcnow()
+
   def toJSON(self):
-    return { "name" : self.name, "rank" : self.rank, "wins": self.wins, "losses": self.losses, "rating": self.rating }
+    return { "name" : self.name, "rank" : self.rank, "wins": self.wins, "losses": self.losses, "rating": self.rating, "parsedDate" : self.parsedDate }
 
 
-class RowParser:
+class PlayerParser:
   def parse(self, row):
-    prow = pq(row)
-    name = prow(".views-field-summoner-name-1").text() 
-    rank = int(prow(".views-field-rank").text().replace(',',''))
-    wins = int(prow(".views-field-wins").text())
-    losses = int(prow(".views-field-losses").text())
-    rating = int(prow(".views-field-rating").text())
+    name = row.find("td", { "class" : re.compile(r"views-field-summoner-name-1")}).string.strip()
+    rank = int(row.find("td", { "class" : re.compile(r"views-field-rank")}).string.replace(',',''))
+    wins = int(row.find("td", { "class" : re.compile(r"views-field-wins")}).string)
+    losses = int(row.find("td", { "class" : re.compile(r"views-field-losses")}).string)
+    rating = int(row.find("td", { "class" : re.compile("views-field-rating")}).string)
     return Player(name, rank, wins, losses, rating)
 
 class DBSaver():
   def __init__(self, host, port, database):
-    connection = Connection('localhost', 27017)
-    self.db = connection['lolranking']
+    connection = Connection(host, port)
+    self.db = connection[database]
 
   def save(self, player):
     self.db.players.insert(player.toJSON())
 
 class LoLParser:
-  def __init__(self, url, dbSaver, parseRange, rowParser):
+  def __init__(self, url, dbSaver, parseRange, playerParser):
     self.url = url
     self.dbSaver = dbSaver
     self.parseRange = parseRange
-    self.rowParser = rowParser
+    self.playerParser = playerParser
 
   def run(self):
     for i in self.parseRange: 
       r = requests.get(self.url + str(i))
-      html = pq(r.content)
-      rows = html("tbody tr")
+      soup = BeautifulSoup(r.content)
+      body = soup.find("tbody")
+      rows = body.findAll("tr")
       for row in rows:
-        self.dbSaver.save(self.rowParser.parse(row))
+        self.dbSaver.save(self.playerParser.parse(row))
 
 def __main__():
   dbSaver = DBSaver("localhost", 27017, "lolranking")
-  rowParser = RowParser()
+  playerParser = PlayerParser()
   url = "http://competitive.euw.leagueoflegends.com/ladders/euw/current/rankedsolo5x5?summoner_name=&page="
-  lolParser = LoLParser(url, dbsaver, range(0, 6342), rowParser)
+  lolParser = LoLParser(url, dbSaver, range(0, 6342), playerParser)
   lolParser.run()
 
 
