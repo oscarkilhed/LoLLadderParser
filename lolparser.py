95e850d5f20f36f364dc8ef0794468791642d0f6
Oscar Kilhed <oscar.kilhed@gmail.com>
2011-12-28 13:17:29 +0100

    IOC refactoring

--- lolparser.py
+++ lolparser.py
@@ -2,8 +2,6 @@ import requests
 from pymongo import Connection
 from pyquery import PyQuery as pq
 
-url = "http://competitive.euw.leagueoflegends.com/ladders/euw/current/rankedsolo5x5?summoner_name=&page="
-
 class Player:
   def __init__(self, name, rank, wins, losses, rating):
     self.name = name
@@ -12,31 +10,50 @@ class Player:
     self.losses = losses
     self.rating = rating
   
-  def toDbObject(self):
+  def toJSON(self):
     return { "name" : self.name, "rank" : self.rank, "wins": self.wins, "losses": self.losses, "rating": self.rating }
 
 
-def parse():
-  connection = Connection('localhost', 27017)
-  db = connection['lolranking']
-  players = db.players
-  for i in range(0, 6342): 
-    r = requests.get(url + str(i))
-    html = pq(r.content)
-    rows = html("tbody tr")
-    for row in rows:
-      prow = pq(row)
-      name = prow(".views-field-summoner-name-1").text() 
-      rank = int(prow(".views-field-rank").text().replace(',',''))
-      wins = int(prow(".views-field-wins").text())
-      losses = int(prow(".views-field-losses").text())
-      rating = int(prow(".views-field-rating").text())
-      players.insert(Player(name, rank, wins, losses, rating).toDbObject())
-    if i != 0 and i % 10 == 0:
-      print str(i / float(6342) * 100) + "%"
+class RowParser:
+  def parse(self, row):
+    prow = pq(row)
+    name = prow(".views-field-summoner-name-1").text() 
+    rank = int(prow(".views-field-rank").text().replace(',',''))
+    wins = int(prow(".views-field-wins").text())
+    losses = int(prow(".views-field-losses").text())
+    rating = int(prow(".views-field-rating").text())
+    return Player(name, rank, wins, losses, rating)
+
+class DBSaver():
+  def __init__(self, host, port, database):
+    connection = Connection('localhost', 27017)
+    self.db = connection['lolranking']
+
+  def save(self, player):
+    self.db.players.insert(player.toJSON())
+
+class LoLParser:
+  def __init__(self, url, dbSaver, parseRange, rowParser):
+    self.url = url
+    self.dbSaver = dbSaver
+    self.parseRange = parseRange
+    self.rowParser = rowParser
+
+  def run(self):
+    for i in self.parseRange: 
+      r = requests.get(self.url + str(i))
+      html = pq(r.content)
+      rows = html("tbody tr")
+      for row in rows:
+        self.dbSaver.save(self.rowParser.parse(row))
 
 def __main__():
-  parse()
+  dbSaver = DBSaver("localhost", 27017, "lolranking")
+  rowParser = RowParser()
+  url = "http://competitive.euw.leagueoflegends.com/ladders/euw/current/rankedsolo5x5?summoner_name=&page="
+  lolParser = LoLParser(url, dbsaver, range(0, 6342), rowParser)
+  lolParser.run()
+
 
 if __name__ == "__main__":
   __main__()
