559bbd2c7457b56d3b810b3d48c7297e5c31d456
Oscar Kilhed <oscar.kilhed@gmail.com>
2011-12-30 21:45:15 +0100

    moved the correct mapreduce script file into the project folder /facepalm

---
+++ lolranking.js
@@ -0,0 +1,28 @@
+map = function(){
+  emit({}, {list : [{ rating: this.rating - this.rating % 10, count: 1 }]})
+};
+
+reduce = function(keys, vals){
+  ratings = {}
+  for(var i = 0; i< vals.length; i++){
+    var currentlist = vals[i].list;
+    for(var x = 0; x < currentlist.length; x++){
+      ratings[currentlist[x].rating] = (ratings[currentlist[x].rating] || 0) + currentlist[x].count;
+    }
+  } 
+  list = []
+  for(kvp in ratings){
+    list.push({ rating: kvp, count: ratings[kvp] });
+  }
+  box = {list : list};
+  return box;
+};
+
+var res = db.runCommand({
+  mapreduce: "players",
+  map: map,
+  reduce: reduce,
+  out: "playersPerRating",
+  verbose: true
+  });
+
